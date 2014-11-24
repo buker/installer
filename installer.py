ab65779d65bb1587b9439db26c2687e986783236
lukasz.zulnowski <lzulnowski@gmail.com>
2014-11-24 14:39:17 +0100

    Poprawiam logowanie oraz kolejnosc wykonywania

--- installer.py
+++ installer.py
@@ -1,52 +1,63 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-__author__ = 'lzulnowski'
+__author__ = 'Łukasz Żułnowski'
+__version__ = '1'
 
+#Importowanie bibliotek
 import sys
 import argparse
 import xml.etree.ElementTree as etree
 import logging
 import subprocess
 
-
-from datetime import datetime as date
-##########LOGOWANIE############
+#Konfiguracja logowania
 logging.basicConfig(format='%(asctime)s [%(levelname)s] - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='installer.log', filemode='w', level=logging.DEBUG, stream=sys.stdout )
-###########################
+#Główna funkcja
 def main():
-    logging.info('Rozpoczynam prace instalatora')
+    msg = 'Rozpoczynam prace instalatora'
+    logging.info(msg)
     #Obsługa argumentów
     parser = argparse.ArgumentParser()
     parser.add_argument('env', choices=['ins', 'uat', 'prd'], help='Podaj środowisko')
     parser.add_argument('app', choices=['ksms', 'nsp,', 'sp2k'],help='Podaj nazwe aplikacji aplikacjep')
     parser.add_argument('file', nargs='*', help='Podaj ścieżki plików. Można podać kilka.')
     args = parser.parse_args()
-    ##Generowanie zmiennych
+    #Generowanie zmiennych
     env = args.env
     app = args.app
     files = args.file
-    msg = "Instalator został uruchomiony z parametrami env=%(env)s, app=%(app), file=%(file)s" % {'env': env, 'app': app, 'files': files}
+    msg = "Instalator został uruchomiony z parametrami env=%(env)s, app=%(app)s, files=%(files)s" % {'env': env, 'app': app, 'files': files}
     logging.debug(msg)
 
     #parsowanie configa
-    logging.info('Sprawdzanie czy plik konfiguracyjny istnieje')
+    msg = 'Sprawdzanie czy plik konfiguracyjny istnieje'
+    logging.info(msg)
+    print(msg)
     try:
         tree = etree.parse('./installer.conf')
     except:
-        logging.exception('Brak pliku konfiguracyjnego')
-    logging.info('Rozpoczynam parsowanie pliku konfiguracyjnego')
+        msg = 'Brak pliku konfiguracyjnego'
+        logging.exception(msg)
+        print(msg)
+    msg = 'Rozpoczynam parsowanie pliku konfiguracyjnego'
+    logging.info(msg)
     root = tree.getroot()
-    ###./ENV[@value='env']/APP[@value='app']/SERVER
     pattern = "./ENV[@value=\'%(env)s\']/APP[@value=\'%(app)s\']/SERVER" % {'env': env, 'app': app}
+    msg = "Takim patternetm szukam w configu: "+pattern
+    logging.debug(msg)
     server_list = root.findall(pattern)
-    logging.info("Plik konfiguracyjny sparsowany")
+    msg= "Plik konfiguracyjny sparsowany"
+    logging.info(msg)
+    print(msg)
     for server in server_list:
         msg = "Rozpoczynam wysyłanie zatrzymywanie usługi %(app)s na %(server)s" % {'app': app, 'server': server.text}
         logging.info(msg)
         print(msg)
-        ######Zatrzymywanie usługi
+        #Zatrzymywanie usługi
         path_server = "jboss7@%(server)s" % {'server': server.text}
         path_app = "bash /opt/kontakt/apps/%(app)s/%(app)s.sh stop" % {'app': app}
+        logging.debug(path_server)
+        logging.debug(path_app)
         stop = subprocess.Popen(["ssh", path_server, path_app], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = stop.communicate()
         if output[0].decode('utf8') == []:
@@ -56,9 +67,9 @@ def main():
             logging.info(output[0].decode('utf8'))
             print(output[0].decode('utf8'))
 
-        ##Wysyłka plików.
+        #Wysyłka plików.
         for file in files:
-            msg = 'Wysyłam plik', file, 'na', server.text
+            msg = 'Wysyłam plik '+file+' na '+server.text+' dla aplikacji '+app
             logging.info(msg)
             print(msg)
             path_bii = "jboss7@%(server)s:/opt/kontakt/apps/%(app)s/backup-install/install" % {'server': server.text, 'app': app}
@@ -72,11 +83,15 @@ def main():
             else:
                 logging.info(output[0].decode('utf8'))
                 print(output[0].decode('utf8'))
-        #######Odpalanie backup install
+                msg = "Plik został wysłany z powodzeniem"
+                logging.info(msg)
+                print(msg)
 
-            print('Odpalam backup-install.sh dla pliku', file, 'na serwerze', server.text)
+        #Odpalanie backup install
+            msg= 'Uruchamiam backup-install.sh dla pliku '+file+' na serwerze '+server.text
+            print(msg)
             path_file = "./backup-install/install/%(file)s" % {'file': file}
-            print(path_file)
+            logging.debug(path_file)
             if env == 'ins':
                 path_env = 'ST'
             elif env == 'uat':
@@ -84,6 +99,7 @@ def main():
             elif env == 'prd':
                 path_env = 'PRD'
             path_backup_sh = "cd /opt/kontakt/apps/%(app)s/; bash /opt/kontakt/apps/%(app)s/backup-install.sh -A ./ -e linux,%(path_env)s,linux/%(path_env)s %(path_file)s" % {'app': app, 'path_file': path_file, 'path_env': path_env}
+            logging.debug(path_backup_sh)
             backup_install = subprocess.Popen(['ssh', path_server, path_backup_sh], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             output = backup_install.communicate()
             if output[0].decode('utf8') == []:
@@ -93,8 +109,12 @@ def main():
                 logging.info(output[0].decode('utf8'))
                 print(output[0].decode('utf8'))
 
-        ##########Odpalanie configure
+        #Odpalanie configure
+        msg= "Uruhamiam configure.sh dla aplikacji %(app)s na serwerze %(server)s" % {'app': app, 'server': server.text}
+        print(msg)
+        logging.info(msg)
         path_configure = "cd /opt/kontakt/apps/%(app)s/; bash configure.sh" % {'app': app}
+        logging.debug(path_configure)
         configure = subprocess.Popen(['ssh', path_server,  path_configure], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = configure.communicate()
         if output[0].decode('utf8') == []:
@@ -104,14 +124,27 @@ def main():
             logging.info(output[0].decode('utf8'))
             print(output[0].decode('utf8'))
 
-        #######Odpalanie usługi
+        #Odpalanie usługi
+        msg= "Uruchamiam aplikacje %(app)s na serwerze %(server)s" % {'app': app, 'server': server.text}
+        print(msg)
+        logging.info(msg)
         path_app = "bash /opt/kontakt/apps/%(app)s/%(app)s.sh start" % {'app': app}
+        logging.debug(path_app)
         start = subprocess.Popen(['ssh', path_server, path_app], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = start.communicate()
         if output[0].decode('utf8') == []:
             logging.error(output[1].decode('utf8'))
             print(output[1].decode('utf8'))
+            msg= ("Nie udało się uruchomić aplikacji")
+            print(msg)
+            logging.exception(msg)
         else:
             logging.info(output[0].decode('utf8'))
             print(output[0].decode('utf8'))
+            msg = "Aplikacja została uruchomiona"
+            print(msg)
+            logging.info(msg)
+        msg= "Działanie instalatora zostało zakończone"
+        print(msg)
+        logging.info(msg)
 main()
